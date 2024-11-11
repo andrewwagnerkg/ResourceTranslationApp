1dc994d4476d5cfa50ec8cbf5ade3fb8e37ded9b
Andrei Vagner <andrei.vagner@timelysoft.net>
2024-11-11 23:52:29 +0600

    --

---
+++ Persistence/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class ApplicationDBContext : DbContext, IApplicationDBContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
