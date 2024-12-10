eb46d3ff98f8fc69a301de07317bb56dec1d6d5d
Andrei Vagner <andrei.vagner@timelysoft.net>
2024-12-10 22:51:18 +0600

    receive languages

--- FE/reasource_translation_app/src/Pages/LanguagesPage/index.jsx
+++ FE/reasource_translation_app/src/Pages/LanguagesPage/index.jsx
@@ -1,7 +1,33 @@
+import {useEffect, useState} from "react";
+import {useAPI} from "../../Hooks/UseAPI.js";
+
 function ResourcesPage() {
+const {GetResources} = useAPI();
+const [locales, setLocales] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    }, []);
+
+    async function onAvailable(res) {
+        console.log("OK");
+        console.log(res);
+        setLocales(res.data);
+    }
+
+    async function onError(error) {
+        console.log(error);
+    }
+
+    const fetchData = async () => {
+        await GetResources(onAvailable, onError);
+    }
+
     return (
         <>
-            Language
+            <ul>
+        {locales.map((x) => (<li key={x.id}>{x.name}</li>))}
+            </ul>
         </>
     )
 }
